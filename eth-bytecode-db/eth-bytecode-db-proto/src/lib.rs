8899eb95302d87d67963b9c66458ce56cca3d391
Anchisa Pinyo <anchisan@NBAAP2100004.local>
2024-04-04 11:41:37 +0700

    first commit blockscout-chart

---
+++ eth-bytecode-db/eth-bytecode-db-proto/src/lib.rs
@@ -0,0 +1,15 @@
+#![allow(clippy::derive_partial_eq_without_eq)]
+
+#[cfg(feature = "http-client")]
+pub mod http_client;
+
+pub mod blockscout {
+    pub mod eth_bytecode_db {
+        pub mod v2 {
+            include!(concat!(
+                env!("OUT_DIR"),
+                "/blockscout.eth_bytecode_db.v2.rs"
+            ));
+        }
+    }
+}
